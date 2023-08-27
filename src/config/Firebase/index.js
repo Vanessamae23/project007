33595622f3b72851897ce29898c08a0c5c367679
Puri Virakarin <puri.prv@gmail.com>
2023-08-27 16:27:27 +0800

    Update IOS env and refactor firebase .env file

--- src/config/Firebase/index.js
+++ src/config/Firebase/index.js
@@ -1,21 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getDatabase } from "firebase/database";
-import firebase from "firebase/app";
+import {initializeApp} from 'firebase/app';
+import {getAuth, GoogleAuthProvider} from 'firebase/auth';
+import {getDatabase} from 'firebase/database';
+import firebase from 'firebase/app';
 
-const app = initializeApp( {
-  apiKey: "AIzaSyCthCLpRtnMG4DipEkBAUqHrDzi_H3NjgM",
-  authDomain: "tiktok-1d4b9.firebaseapp.com",
-  databaseURL: "https://tiktok-1d4b9-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "tiktok-1d4b9",
-  storageBucket: "tiktok-1d4b9.appspot.com",
-  messagingSenderId: "1057965972778",
-  appId: "1:1057965972778:web:8e44b479427f68b7475253",
-  measurementId: "G-TDRPMJ1JVD"
-})
-
+const app = initializeApp({
+  apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.PUBLIC_MEASUREMENT_ID,
+});
 
 export const auth = getAuth(app);
 export const db = getDatabase(app);
 // export default Firebase
-
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,39 +1,55 @@
-import { StyleSheet, ScrollView, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { getData } from '../../utils/localStorage'
-import { Card, Header } from '../../components'
+import {StyleSheet, ScrollView, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {getData} from '../../utils/localStorage';
+import {Card, Header, Button} from '../../components';
 
 export default function Home() {
-  const [ username, setUsername ] =useState('')
+  const [username, setUsername] = useState('');
   useEffect(() => {
     getData('user').then(res => {
       const data = res;
-      setUsername(data.fullName)
-    })
-  }, [])
+      setUsername(data.fullName);
+    });
+  }, []);
+
+  const handleTopUp = () => {};
+
+  const handleTransfer = () => {};
+
   return (
     <ScrollView style={styles.page}>
-        <Header title="Good day," subtitle={username} />
-        <Card name={username} />
+      <Header title="Good day," subtitle={username} />
+      <Card name={username} />
       <Text style={styles.welcome}>Actions</Text>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-evenly',
+
+        }}>
+        <Button text="Top Up" onPress={handleTopUp} />
+        <Button text="Transfer" onPress={handleTransfer} />
+      </View>
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    welcome: {
-        fontSize: 22,
-        marginTop: 30,
-        marginBottom: 15,
-        maxWidth: 300,
-        fontWeight: "600",
-        color: 'black',
-        fontWeight: '600'
-    },
-    page: {
-        paddingTop: 30,
-        paddingHorizontal: 16,
-        backgroundColor: "white",
-        flex: 1
-    },
-})
+  welcome: {
+    fontSize: 22,
+    marginTop: 30,
+    marginBottom: 15,
+    maxWidth: 300,
+    fontWeight: '600',
+    color: 'black',
+    fontWeight: '600',
+  },
+  page: {
+    paddingTop: 30,
+    paddingHorizontal: 16,
+    backgroundColor: 'white',
+    flex: 1,
+  },
+});

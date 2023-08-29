a9494d6059c59bb8126d7b5febd063f3bc3b8181
Puri Virakarin <puri.prv@gmail.com>
2023-08-29 15:29:34 +0800

    Integrate stripe into react-native (need work on this)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import FlashMessage from 'react-native-flash-message';
 import {Provider, useSelector} from 'react-redux';
 import store from './redux/store';
 import {LogBox} from 'react-native';
+// import {StripeProvider} from '@stripe/stripe-react-native';
 //import { Loading } from './components';
 LogBox.ignoreLogs([
   'Warning: Async Storage has been extracted from react-native core',
@@ -14,11 +15,17 @@ const MainApp = () => {
   const stateGlobal = useSelector(state => state);
   return (
     <>
+      {/* <StripeProvider
+        publishableKey={process.env.STRIPE_PUBLISHABLE_KEY ?? ''}
+        merchantIdentifier="merchant.identifier" // required for Apple Pay
+        urlScheme="your-url-scheme" // required for 3D Secure and bank redirects
+      > */}
       <NavigationContainer>
         <Router />
       </NavigationContainer>
       <FlashMessage position="top" />
       {/* {stateGlobal.loading && <Loading />} */}
+      {/* </StripeProvider> */}
     </>
   );
 };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,9 +2,17 @@ import {StyleSheet, ScrollView, Text, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {getData} from '../../utils/localStorage';
 import {Card, Header, Button} from '../../components';
+import {useStripe} from '@stripe/stripe-react-native';
+import {useHttpsCallable} from '../../utils/useHttpsCallable';
 
 export default function Home() {
   const [username, setUsername] = useState('');
+
+  const createPaymentIntent = useHttpsCallable('createPaymentIntent');
+  // console.log('createPaymentIntent: ', createPaymentIntent);
+
+  const {initPaymentSheet, presentPaymentSheet} = useStripe();
+
   useEffect(() => {
     getData('user').then(res => {
       const data = res;
@@ -12,9 +20,62 @@ export default function Home() {
     });
   }, []);
 
-  const handleTopUp = () => {};
+  const createTenant = async () => {
+    // add up the product totals TBC
+    const amount = 20;
 
-  const handleTransfer = () => {};
+    // if (amount > 0) {
+    // call the cloud function to create a new
+    // Payment Intent with the calculated total
+    // const paymentIntent = await createPaymentIntent.call({
+    //   amount: amount * 100,
+    // });
+    //storeData('paymentIntent', paymentIntent);
+    // }
+
+    const response = await fetch(
+      'http://localhost:5001/tiktok/us-central1/createPaymentIntent',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          data: {
+            amount: 999,
+          },
+        }),
+      },
+    );
+
+    // console.log('Response: ', response._bodyBlob._data.blobId);
+
+    // const paymentIntent = response._bodyBlob._data.blobId;
+    console.log('paymentIntent: ', paymentIntent); 
+
+    const paymentIntent = "pi_3Nk4hxDNoOorWK5q1xwFjliW_secret_MpKzL03bt6wOZcgqrKsQL62OJ";
+
+    const initResponse = await initPaymentSheet({
+      merchantDisplayName: 'notJust.dev',
+      paymentIntentClientSecret: paymentIntent,
+    });
+    if (initResponse.error) {
+      console.log(initResponse.error);
+      // Alert.alert('Something went wrong');
+      return;
+    }
+
+    const paymentResponse = await presentPaymentSheet();
+
+    if (paymentResponse.error) {
+      // Alert.alert(
+      //   `Error code: ${paymentResponse.error.code}`,
+      //   paymentResponse.error.message,
+      // );
+      return;
+    }
+    return;
+  };
 
   return (
     <ScrollView style={styles.page}>
@@ -27,10 +88,9 @@ export default function Home() {
           flexDirection: 'row',
           alignItems: 'center',
           justifyContent: 'space-evenly',
-
         }}>
-        <Button text="Top Up" onPress={handleTopUp} />
-        <Button text="Transfer" onPress={handleTransfer} />
+        <Button text="createTenant" onPress={createTenant} />
+        <Button text="Transfer" onPress={createTenant} />
       </View>
     </ScrollView>
   );

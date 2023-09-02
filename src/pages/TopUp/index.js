e1947c854a486ac0f59f6d4fc7cf4527ac2f0966
Puri Virakarin <puri.prv@gmail.com>
2023-09-02 11:46:36 +0800

    Set up OTP Email verification for high-volumn topup

--- src/pages/TopUp/index.js
+++ src/pages/TopUp/index.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Alert} from 'react-native';
 import React, {useState, useCallback, useEffect} from 'react';
 import {colors, showSuccess, useForm} from '../../utils';
 import {Button, Gap, Input} from '../../components';
@@ -9,12 +9,25 @@ import {CardField, useStripe} from '@stripe/stripe-react-native';
 import Config from 'react-native-config';
 
 const TopUp = ({navigation}) => {
+  const [otp, setOtp] = useState('');
+  const [showOtpInput, setShowOtpInput] = useState(false);
   const [amount, setAmount] = useState(0);
   const [pin, setPin] = useState(0);
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   const balance = useSelector(state => state.balance.value);
-  const handleTopup = useCallback(() => {
+
+  const handleHighTopUp = () => {
+    setShowOtpInput(true);
+    fetch(`http://${Config.NODEJS_URL}:${Config.NODEJS_PORT}/email/send`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+
+  const processPayment = () => {
     const billingDetails = {
       email: 'email@stripe.com',
     };
@@ -46,13 +59,43 @@ const TopUp = ({navigation}) => {
           },
         ).then(resp => {
           let final = balance + amount;
-          showSuccess('Added S$' + amount+ ' to your account');
+          showSuccess('Added S$' + amount + ' to your account');
           dispatch(setBalance(final));
+          navigation.navigate('Home');
         });
       })
       .catch(err => {
         console.log(err);
       });
+  }
+
+  const handleOtpSubmit = useCallback(() => {
+    fetch(`http://${Config.NODEJS_URL}:${Config.NODEJS_PORT}/email/verify`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        otp: otp,
+      }),
+    }).then(res => {
+      if (res.status === 200) {
+        setShowOtpInput(false);
+        Alert.alert('Success', 'OTP verified successfully.');
+        processPayment();
+        return;
+      } else {
+        Alert.alert('Error', 'Incorrect OTP. Please try again.');
+      }
+    });
+  }, [otp]);
+
+  const handleTopup = useCallback(() => {
+    if (amount > 1000) {
+      handleHighTopUp();
+      return;
+    }
+    processPayment();
   }, [amount]);
   const handleNumber = useCallback(
     handler => value => {
@@ -70,52 +113,78 @@ const TopUp = ({navigation}) => {
         <Text style={styles.balance}>Current Balance</Text>
         <Text style={styles.price}>S${balance}</Text>
       </View>
-      <View style={styles.container}>
-        <Text style={styles.head}>Top Up</Text>
-        <Gap height={20} />
-        <View style={{width: '100%'}}>
-          <Input
-            fullWidth={true}
-            onNumber
-            onChangeText={handleNumber(setAmount)}
-            label="Amount"
+      {!showOtpInput && (
+        <View style={styles.container}>
+          <Text style={styles.head}>Top Up</Text>
+          <Gap height={20} />
+          <View style={{width: '100%'}}>
+            <Input
+              fullWidth={true}
+              onNumber
+              onChangeText={handleNumber(setAmount)}
+              label="Amount"
+            />
+          </View>
+          <Gap height={20} />
+          <View style={{width: '100%'}}>
+            <Input
+              secureTextEntry={true}
+              fullWidth={true}
+              onNumber={handleNumber(setPin)}
+              label="Pin Number"
+            />
+          </View>
+          <Gap height={30} />
+          <View>
+            <CardField
+              style={{
+                height: 30,
+              }}
+              postalCodeEnabled={false}
+              placeholder={{
+                number: '4242 4242 4242 4242',
+              }}
+              onCardChange={cardDetails => {}}
+            />
+          </View>
+          <Gap height={30} />
+          <Button
+            textColor={colors.black}
+            color={colors.secondary}
+            onPress={handleTopup}
+            text="Top Up"></Button>
+          <Gap height={20} />
+          <Button
+            textColor={colors.black}
+            color={colors.secondary}
+            onPress={() => navigation.goBack()}
+            text="Back"></Button>
+        </View>
+      )}
+      {showOtpInput && (
+        <View style={styles.container}>
+          <Text style={styles.head}>Enter OTP</Text>
+          <Gap height={5} />
+          <Text style={styles.subhead}>OTP has been already sent.</Text>
+          <Text style={styles.subhead}>Please check your email.</Text>
+          <Gap height={20} />
+          <View style={{width: '100%'}}>
+            <Input
+              fullWidth={true}
+              onNumber
+              onChangeText={setOtp}
+              label="OTP"
+            />
+          </View>
+          <Gap height={30} />
+          <Button
+            text="Submit OTP"
+            textColor={colors.black}
+            color={colors.secondary}
+            onPress={handleOtpSubmit}
           />
         </View>
-        <Gap height={20} />
-        <View style={{width: '100%'}}>
-          <Input
-            secureTextEntry={true}
-            fullWidth={true}
-            onNumber={handleNumber(setPin)}
-            label="Pin Number"
-          />
-        </View>
-        <Gap height={20} />
-        <View>
-          <CardField
-            style={{
-              height: 30,
-            }}
-            postalCodeEnabled={false}
-            placeholder={{
-              number: '4242 4242 4242 4242',
-            }}
-            onCardChange={cardDetails => {}}
-          />
-        </View>
-        <Gap height={30} />
-        <Button
-          textColor={colors.black}
-          color={colors.secondary}
-          onPress={handleTopup}
-          text="Top Up"></Button>
-        <Gap height={20} />
-        <Button
-          textColor={colors.black}
-          color={colors.secondary}
-          onPress={() => navigation.goBack()}
-          text="Back"></Button>
-      </View>
+      )}
     </View>
   );
 };
@@ -138,14 +207,20 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     textAlign: 'center',
   },
+  subhead: {
+    color: colors.white,
+    fontSize: 16,
+    fontWeight: '300',
+    textAlign: 'center',
+  },
   container: {
     backgroundColor: colors.primary,
-    paddingVertical: 50,
+    paddingVertical: 40,
     borderTopEndRadius: 30,
     borderTopStartRadius: 30,
     paddingHorizontal: 30,
     width: '100%',
-    flex: 3,
+    flex: 3.5,
     display: 'flex',
     flexDirection: 'column',
   },

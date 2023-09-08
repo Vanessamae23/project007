89725119cc514dc22c75fdec0d7a69b0320c7043
Puri Virakarin <puri.prv@gmail.com>
2023-09-08 23:59:38 +0800

    Edit Guardian UI

--- src/components/atoms/Contact/index.js
+++ src/components/atoms/Contact/index.js
@@ -1,70 +1,85 @@
 import React from 'react';
-import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
-import { colors } from '../../../utils';
+import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+import {colors} from '../../../utils';
 
-export const Contactlist = ({ title, contacts, onContactPress }) => (
-    <View accessible={false} style={styles.contactList}>
-        <View
-            style={{ width: '100%' }}
-        >
-            <Text style={styles.listTitle}>{title.toUpperCase()}</Text>
-        </View>
-        {contacts.map(contact => (
-            <Contact key={contact.uid} {...contact} onPress={() => onContactPress(contact)} />
-        ))}
+export const Contactlist = ({
+  title,
+  contacts,
+  onContactPress,
+  selectedContact,
+}) => (
+  <View accessible={false} style={styles.contactList}>
+    <View style={{width: '100%'}}>
+      <Text style={styles.listTitle}>{title.toUpperCase()}</Text>
     </View>
+    {contacts.map(contact => (
+      <Contact
+        key={contact.uid}
+        {...contact}
+        onPress={() => onContactPress(contact)}
+        isSelectedContact={
+          selectedContact && selectedContact.uid === contact.uid
+        }
+      />
+    ))}
+  </View>
 );
 
-export const Contact = ({ name, uid, onPress }) => (
-    <TouchableOpacity
-        style={styles.container}
-        onPress={onPress}
-    >
-        <View style={styles.avatar}>
-            <Text style={styles.avaText}>{name[0].toUpperCase()}</Text>
-        </View>
-        <View style={styles.details}>
-            <Text style={styles.name}>{name}</Text>
-            <Text style={styles.number}>{uid}</Text>
-        </View>
-    </TouchableOpacity>
+export const Contact = ({name, uid, onPress, isSelectedContact}) => (
+  <TouchableOpacity
+    style={[
+      styles.container,
+      isSelectedContact && {backgroundColor: colors.secondary},
+    ]}
+    onPress={onPress}>
+    <View style={styles.avatar}>
+      <Text style={styles.avaText}>{name[0].toUpperCase()}</Text>
+    </View>
+    <View style={styles.details}>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.number}>{uid}</Text>
+    </View>
+  </TouchableOpacity>
 );
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        paddingVertical: 10,
-        height: 60,
-        width: '100%',
-    },
-    avatar: {
-        height: 40,
-        width: 40,
-        backgroundColor: colors.primary,
-        borderRadius: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    avaText: {
-        fontSize: 25,
-        color: colors.white,
-    },
-    details: {
-        marginLeft: 10,
-        justifyContent: 'space-around',
-    },
-    contactList: {
-        padding: 10,
-        width: '100%',
-    },
-    listTitle: {
-        color: colors.primary,
-    },
-    name: {
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    number: {
-        fontSize: 14,
-    },
+  container: {
+    flexDirection: 'row',
+    paddingVertical: 10,
+    height: 60,
+    width: '100%',
+  },
+  selectedContact: {
+    backgroundColor: colors.primary,
+  },
+  avatar: {
+    height: 40,
+    width: 40,
+    backgroundColor: colors.primary,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  avaText: {
+    fontSize: 25,
+    color: colors.white,
+  },
+  details: {
+    marginLeft: 10,
+    justifyContent: 'space-around',
+  },
+  contactList: {
+    padding: 10,
+    width: '100%',
+  },
+  listTitle: {
+    color: colors.primary,
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  number: {
+    fontSize: 14,
+  },
 });
--- src/pages/Guardian/index.js
+++ src/pages/Guardian/index.js
@@ -1,79 +1,230 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import { Button, Gap, Input } from '../../components'
-import { colors, useForm } from '../../utils'
-import Config from 'react-native-config'
+import {StyleSheet, Text, View, TextInput, ScrollView} from 'react-native';
+import React, {useState, useEffect, useRef, useCallback} from 'react';
+import {Button, Gap, Input} from '../../components';
+import {Contactlist} from '../../components/atoms/Contact';
+import {colors, useForm, showError} from '../../utils';
+import Config from 'react-native-config';
 
 const Guardian = ({navigation}) => {
-    const [form, setForm] = useForm({
-        guardianName: ''
-    });
-    const [guardian, setGuardian] = useState('')
-    const [users, setUsers] = useState([])
-    const handleSearch = () => {
-        fetch(`http://${Config.NODEJS_URL}:${Config.NODEJS_PORT}/payments/find-users?email=${form.guardianName}`)
-            .then(res => res.json())
-            .then(res => {
-                setUsers(res.users)
-        });
-    }
+  const [form, setForm] = useForm({
+    guardianName: '',
+  });
+  const [guardian, setGuardian] = useState('');
+  const [selectedContact, setSelectedContact] = useState('');
+  const [query, setQuery] = useState('');
+  const [isQueryJustChanged, setIsQueryJustChanged] = useState(false);
+  const queryJustChangedTimer = useRef(null);
+  const [contacts, setContacts] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
+  const [password, setPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
 
-    const handleAdd = () => {
+  const handleContactPress = contact => {
+    setSelectedContact(contact);
+  };
+
+  const isValidPhoneNumber = number => {
+    return number.length <= 8 && /^\d+$/.test(number);
+  };
+
+  const handleSearchContact = useCallback(
+    query => {
+      if (!isSearching && query.length >= 2) {
         fetch(
-            `http://${Config.NODEJS_URL}:${Config.NODEJS_PORT}/guardian`,
-            {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                user_uid: users[0].uid
-              }),
-            },
-          )
+          `http://${Config.NODEJS_URL}:${Config.NODEJS_PORT}/payments/find-users?name=${query}`,
+        )
+          .then(res => res.json())
+          .then(res => {
+            setIsSearching(false);
+            setContacts(res.users);
+          })
+          .catch(() => {
+            showError('failed to find users');
+          });
+      }
+    },
+    [isSearching],
+  );
+
+  const handleAdd = () => {
+    if (selectedContact) {
+      fetch(
+        `http://${Config.NODEJS_URL}:${Config.NODEJS_PORT}/auth/set-guardian`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            guardianId: selectedContact.uid,
+            newPassword: newPassword,
+            password: password,
+          }),
+        },
+      )
+        .then(res => {
+          if (res.status == 200) {
+            setGuardian(selectedContact.name);
+            setSelectedContact('');
+            setNewPassword('');
+            setPassword('');
+            navigation.navigate('Home');
+          }
+          return res.json();
+        })
+        .then(res => {
+          if (res.error) {
+            showError(res.error);
+          }
+        });
+    } else {
+      showError('No contact selected');
     }
+  };
+
+  useEffect(() => {
+    fetch(`http://${Config.NODEJS_URL}:${Config.NODEJS_PORT}/auth/is-guarded`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(res => {
+        if (res.message != '') {
+          setGuardian(res.message);
+        }
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!isQueryJustChanged) {
+      handleSearchContact(query);
+    }
+  }, [query, isQueryJustChanged]);
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
+      <Gap height={40} />
       <Text style={styles.title}>Add Guardian</Text>
       <Gap height={16} />
-        <Input
-          label="Guardian Name"
-          value={form.guardianName}
-          onChangeText={value => setForm('guardianName', value)}
-          placeholder="Type your guardian's name"
-        />
-        <Gap height={50} />
-        {users.map((user, index) => (
-            <View style={{ padding: 20, backgroundColor: colors.primary, borderWidth: 1, borderRadius: 20}}>
-                <Text style={{color : colors.white}} key={index}>Add {user.name} as Guardian</Text>
-            </View>
-            
-          ))}
-          <Gap height={36} />
-        <Button text="Search" onPress={handleSearch}/>
+      <Text style={{fontWeight: 'bold'}}>Search the name of the guardian</Text>
+      <Gap height={7} />
+      <TextInput
+        label="Guardian Name"
+        style={styles.search}
+        placeholder="Search name or enter number"
+        onChangeText={text => {
+          setQuery(text);
+          setIsQueryJustChanged(true);
+          clearTimeout(queryJustChangedTimer.current);
+          queryJustChangedTimer.current = setTimeout(() => {
+            setIsQueryJustChanged(false);
+          }, 1000);
+        }}
+      />
+      <Gap height={30} />
+      {contacts.length > 0 ? (
+        <ScrollView>
+          <Contactlist
+            title="All contacts"
+            contacts={contacts}
+            onContactPress={handleContactPress}
+            selectedContact={selectedContact}
+          />
+        </ScrollView>
+      ) : isValidPhoneNumber(query) ? (
+        <ScrollView>
+          <Contactlist
+            title="Unknown contact"
+            contacts={[{name: 'Unknown', number: query}]}
+            onContactPress={handleContactPress}
+            selectedContact={selectedContact}
+          />
+        </ScrollView>
+      ) : (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{color: colors.black || 'black'}}>
+            No contacts found
+          </Text>
+        </View>
+      )}
+      <Gap height={50} />
+      {/* <Button text="Search" onPress={handleSearch} /> */}
+      {guardian != '' && (
+        <>
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: '300',
+            }}>{`Current Guardian: ${guardian}`}</Text>
+          <Gap height={20} />
+          <Text style={{fontWeight: 'bold'}}>
+            Password for Current Guardian
+          </Text>
+          <Gap height={7} />
+          <TextInput
+            label="Old Password"
+            secureTextEntry={true}
+            style={styles.search}
+            value={password}
+            placeholder="fill the current password"
+            onChangeText={text => {
+              setPassword(text);
+            }}
+          />
+          <Gap height={20} />
+        </>
+      )}
+      <Text style={{fontWeight: 'bold'}}>New Password</Text>
+      <Gap height={7} />
+      <TextInput
+        label="New Password"
+        style={styles.search}
+        secureTextEntry={true}
+        placeholder="fill new Password"
+        value={newPassword}
+        onChangeText={text => {
+          setNewPassword(text);
+        }}
+      />
+      <Gap height={30} />
+      <Button
+        disabled={!selectedContact}
+        text="Change Guardian"
+        onPress={handleAdd}
+      />
       <Gap height={16} />
-      <Button disabled={users.length == 0}  text="Add Guardian" onPress={handleAdd}/>
-      <Gap height={16} />
-      <Button text="Back" onPress={() => navigation.navigate('Home')} />
-    </View>
-  )
-}
+      <Button
+        text="Back"
+        onPress={() => {
+          setSelectedContact('');
+          setNewPassword('');
+          setPassword('');
+          navigation.navigate('Home');
+        }}
+      />
+    </ScrollView>
+  );
+};
 
-export default Guardian
+export default Guardian;
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'white',
-        paddingTop: 26,
-        flex: 1,
-        height: '100%',
-        paddingHorizontal: 30,
-        backgroundColor: colors.white,
-      },
-    title: {
-      paddingTop: 10,
-      fontSize: 20,
-      textAlign: 'top',
-      fontWeight: '500',
-    },
+  container: {
+    backgroundColor: 'white',
+    paddingTop: 26,
+    flex: 1,
+    height: '100%',
+    paddingHorizontal: 30,
+  },
+  title: {
+    paddingTop: 10,
+    fontSize: 20,
+    textAlign: 'left',
+    fontWeight: '500',
+  },
+  input: {
+    color: 'black',
+    textColor: 'black',
+  },
 });
